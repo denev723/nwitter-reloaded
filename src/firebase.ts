ac3e2d0a15ee2c405aac097cf25a3370ef26d2b6
denev <denev723@gmail.com>
2024-10-16 02:00:10 +0900

    setting

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCj5rIzGe95blMf81lLd4P9vhlAdUy_r5Y",
+  authDomain: "nwitter-reloaded-77433.firebaseapp.com",
+  projectId: "nwitter-reloaded-77433",
+  storageBucket: "nwitter-reloaded-77433.appspot.com",
+  messagingSenderId: "264750539440",
+  appId: "1:264750539440:web:5927999525f986f5c95175",
+};
+
+const app = initializeApp(firebaseConfig);
