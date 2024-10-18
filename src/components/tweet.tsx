4b633efd93ff6fde6fe31a11b120e55314b270fe
denev <denev723@gmail.com>
2024-10-19 01:56:19 +0900

    create profile, edit tweet & edit displayName code challenge

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, database, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
-import { deleteObject, ref } from "firebase/storage";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
 import { useState } from "react";
 
 const Wrapper = styled.div`
@@ -44,6 +49,7 @@ const DeleteButton = styled.button`
 `;
 
 const EditButton = styled.button`
+  margin-left: 5px;
   background-color: #1d9bf0;
   color: white;
   font-weight: 600;
@@ -55,17 +61,105 @@ const EditButton = styled.button`
   cursor: pointer;
 `;
 
-const EditForm = styled.div``;
-const EditTextarea = styled.textarea``;
-const BtnWrap = styled.div``;
-const DeletePhotoBtn = styled.button``;
-const UpdatePhotoBtn = styled.button``;
-const EditSubmitBtn = styled.input``;
+const EditForm = styled.form``;
+const EditTextarea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const FlexWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
+const BtnWrap = styled.div`
+  * {
+    &:not(:last-child) {
+      margin-right: 5px;
+    }
+  }
+`;
+const DeletePhotoBtn = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const UpdatePhotoBtn = styled.label`
+  background-color: #1d9bf0;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const UpdatePhotoFile = styled.input`
+  display: none;
+`;
+const EditSubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const CancelBtn = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const Error = styled.p`
+  color: tomato;
+  margin-top: 10px;
+`;
 
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
+  const [isLoading, setIsLoading] = useState(false);
   const [edit, setEdit] = useState(false);
+  const [editTweet, setEditTweet] = useState("");
+  const [updateFile, setUpdateFile] = useState<File | null>(null);
+  const [error, setError] = useState("");
+
   const onDelete = async () => {
+    if (edit) {
+      setEdit(false);
+    }
     const ok = confirm("Are you sure you want to delete thie tweet?");
     if (!ok || user?.uid !== userId) return;
     try {
@@ -80,11 +174,75 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
       //
     }
   };
-  const onEdit = () => {
+
+  const onSetEdit = () => {
     if (edit) {
       setEdit(false);
     } else {
+      setEditTweet(tweet);
       setEdit(true);
+    }
+  };
+
+  const onDeletePhoto = async () => {
+    const ok = confirm("Are you sure delete image?");
+    if (!ok && !photo) return;
+    try {
+      const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+      await deleteObject(photoRef);
+      await updateDoc(doc(database, "tweets", id), {
+        photo: "",
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setEdit(true);
+    }
+  };
+
+  const onEditChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditTweet(e.target.value);
+  };
+
+  const onUpdateFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length > 0) {
+      if (files[0].size > 1 * 1024 * 1024) {
+        setError("파일의 크기는 최대 1Mb가 넘을 수 없습니다.");
+        return;
+      }
+      setUpdateFile(files[0]);
+    }
+  };
+
+  const onEditSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || editTweet === "" || editTweet.length > 180)
+      return;
+    try {
+      await updateDoc(doc(database, "tweets", id), {
+        tweet: editTweet,
+      });
+      if (updateFile) {
+        if (photo) {
+          const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+          await deleteObject(photoRef);
+        }
+        const locationRef = ref(storage, `tweets/${user.uid}/${id}`);
+        const result = await uploadBytes(locationRef, updateFile);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc(database, "tweets", id), {
+          photo: url,
+        });
+      }
+      setEditTweet(tweet);
+      setUpdateFile(null);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+      setEdit(false);
     }
   };
   return (
@@ -96,20 +254,39 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
           {user?.uid === userId ? (
             <>
               <DeleteButton onClick={onDelete}>Delete</DeleteButton>
-              <EditButton onClick={onEdit}>Edit</EditButton>
+              <EditButton onClick={onSetEdit}>Edit</EditButton>
             </>
           ) : null}
         </Column>
         <Column>{photo ? <Photo src={photo} /> : null}</Column>
       </Wrapper>
       {edit ? (
-        <EditForm>
-          <EditTextarea value={tweet} />
-          <BtnWrap>
-            <DeletePhotoBtn>Delete Photo</DeletePhotoBtn>
-            <UpdatePhotoBtn>Update Photo</UpdatePhotoBtn>
-          </BtnWrap>
-          <EditSubmitBtn type="submit" value="Edit Tweet" />
+        <EditForm onSubmit={onEditSubmit}>
+          <EditTextarea onChange={onEditChange} rows={5} value={editTweet} />
+          <FlexWrap>
+            <BtnWrap>
+              <DeletePhotoBtn onClick={onDeletePhoto}>
+                Delete Photo
+              </DeletePhotoBtn>
+              <UpdatePhotoBtn htmlFor="updateFile">
+                {updateFile ? "Uploaded" : "Update Photo"}
+              </UpdatePhotoBtn>
+              <UpdatePhotoFile
+                onChange={onUpdateFileChange}
+                type="file"
+                id="updateFile"
+                accept="image/*"
+              />
+            </BtnWrap>
+            <BtnWrap>
+              <EditSubmitBtn
+                type="submit"
+                value={isLoading ? "Update Posting..." : "Edit Tweet"}
+              />
+              <CancelBtn onClick={onSetEdit}>Cancel</CancelBtn>
+            </BtnWrap>
+          </FlexWrap>
+          {error ? <Error>{error}</Error> : null}
         </EditForm>
       ) : null}
     </>
--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,207 @@
+import styled from "styled-components";
+import { auth, database, storage } from "../firebase";
+import { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweet";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const AvatarUpload = styled.label`
+  width: 80px;
+  height: 80px;
+  overflow: hidden;
+  border-radius: 50%;
+  background-color: #1d9df0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+
+const AvatarInput = styled.input`
+  display: none;
+`;
+
+const NameWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Name = styled.span`
+  display: inline-block;
+  margin-right: 10px;
+  font-size: 20px;
+`;
+
+const EditBtn = styled.button`
+  width: 20px;
+  background-color: transparent;
+  padding: 0;
+  border: none;
+  cursor: pointer;
+  svg {
+    width: 15px;
+    fill: white;
+  }
+`;
+
+const Tweets = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const EditForm = styled.form``;
+
+const EditInput = styled.input``;
+
+const EditSubmit = styled.input``;
+
+const CancelBtn = styled.button``;
+
 export default function Profile() {
-    return <h1>Profile!</h1>
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [edit, setEdit] = useState(false);
+  const [newName, setNewName] = useState(user?.displayName);
+
+  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewName(e.target.value);
+  };
+
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user?.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+
+  const onEditClick = () => {
+    if (edit) {
+      setEdit(false);
+    } else {
+      setEdit(true);
+    }
+  };
+
+  const onEditSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!user) return;
+    try {
+      await updateProfile(user, {
+        displayName: newName,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setEdit(false);
+    }
+  };
+
+  const fetchTweets = async () => {
+    const TweetQuery = query(
+      collection(database, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(25)
+    );
+    const snapshot = await getDocs(TweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      {edit ? (
+        <EditForm onSubmit={onEditSubmit}>
+          <EditInput type="text" value={newName} onChange={onNameChange} />
+          <EditSubmit type="submit" />
+          <CancelBtn onClick={onEditClick}>취소</CancelBtn>
+        </EditForm>
+      ) : (
+        <NameWrap>
+          <Name>{user.displayName ?? "Anonymous"}</Name>
+          <EditBtn onClick={onEditClick}>
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z" />
+            </svg>
+          </EditBtn>
+        </NameWrap>
+      )}
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
+    </Wrapper>
+  );
 }
